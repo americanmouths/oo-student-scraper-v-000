8bbbc3d6d8fbcb776a086d0ea907e9925162113f
Kayla Hansen <shiverr@gmail.com>
2017-11-04 21:49:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
 
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each{|key, value| self.send("#{key}="), value)}
+    attributes_hash.each{|key, value| self.send(("#{key}="), value)}
     self
   end
 
