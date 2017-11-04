adfbcae451918a450ae68943fd70781be74b29f6
Kayla Hansen <shiverr@gmail.com>
2017-11-04 21:12:36 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,22 +21,21 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     scraped_profiles = {}
-    binding.pry
     doc.css(".main-wrapper").each do |profile|
       if profile.css(".vitals-container .social-icon-container a").attribute("href").value.include?("twitter")
         scraped_profiles[:twitter] = profile.css(".vitals-container .social-icon-container a").attribute("href").value
-        
+
       elsif profile.css(".vitals-container .social-icon-container a + a").attribute("href").value.include?("linkedin")
         scraped_profiles[:linkedin] = profile.css(".vitals-container .social-icon-container a + a").attribute("href").value
-        
-      elsif profile.css(".vitals-container .social-icon-container a + a + a").attribute("href").value.include?("github")
-        scraped_profiles[:github] = profile.css(".vitals-container .social-icon-container a + a + a").attribute("href").value 
 
-      else scraped_profiles[:blog] = profile.css(".vitals-container .social-icon-container a + a + a + a").attribute("href").value 
+      elsif profile.css(".vitals-container .social-icon-container a + a + a").attribute("href").value.include?("github")
+        scraped_profiles[:github] = profile.css(".vitals-container .social-icon-container a + a + a").attribute("href").value
+
+      else scraped_profiles[:blog] = profile.css(".vitals-container .social-icon-container a + a + a + a").attribute("href").value
     end
-    
-    scraped_profiles[:profile_quote] = profile.css(".vitals-text-container .profile-quote").text 
-    scraped_profiles[:bio] = profile.css(".description-holder p").first.text 
+
+    scraped_profiles[:profile_quote] = profile.css(".vitals-text-container .profile-quote").text
+    scraped_profiles[:bio] = profile.css(".description-holder p").first.text
   end
   scraped_profiles
 end
